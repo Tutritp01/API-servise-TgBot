76f0ee00c95d923cb942018c2f4aa0669e73bfb6
Sav4ykVladzislau <44sav4yk@gmail.com>
2023-02-02 23:00:17 +0300

    Create CustomerPersistence interface

---
+++ persistence-core/src/main/java/com/tutrit/persistence/core/persistence/CustomerPersistence.java
@@ -0,0 +1,9 @@
+package com.tutrit.persistence.core.persistence;
+
+import com.tutrit.persistence.core.bean.Customer;
+
+
+public interface CustomerPersistence {
+    Customer save(Customer customer);
+    Customer findById(String id);
+}
