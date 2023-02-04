432439c20598389d2728242febd396d8fdb65eb7
Sav4ykVladzislau <44sav4yk@gmail.com>
2023-02-05 00:37:47 +0300

    Add InMemoryPersistence module and change parent pom.xml

---
+++ in-memory-repository/src/main/java/com/tutrit/persistence/InMemoryUserPersistence.java
@@ -0,0 +1,26 @@
+package com.tutrit.persistence;
+
+import com.tutrit.persistence.core.bean.User;
+import com.tutrit.persistence.core.persistence.UserPersistence;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@Component
+public class InMemoryUserPersistence implements UserPersistence {
+    private final static Map<String, User> userMap = new HashMap<>();
+
+    @Override
+    public User save(User user) {
+        user.setId(UUID.randomUUID().toString());
+        userMap.put(user.getId(), user);
+        return user;
+    }
+
+    @Override
+    public User findById(String id) {
+        return userMap.get(id);
+    }
+}
