5e51bf1e0737ef9d5e9870908d2b4945e6575746
yurasava <yurasava@gmail.com>
2023-02-04 18:39:54 +0300

    TGBOT-281 added tests

---
+++ rest-api-service/src/test/com/tutrit/storestapiservice/service/CarServiceTest.java
@@ -0,0 +1,63 @@
+package com.tutrit.storestapiservice.service;
+
+import com.tutrit.persistence.core.bean.Car;
+import com.tutrit.storestapiservice.client.CarClient;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+class CarServiceTest {
+
+    @Autowired
+    CarService carService;
+
+    @MockBean
+    CarClient carClient;
+
+    @Test
+    void save() {
+        var car = makeVictim();
+        when(carClient.save(makeCarBeforeSave())).thenReturn(makeExpected());
+        var actualCar = carService.save(car);
+        assertEquals(makeExpected(), actualCar);
+    }
+
+    @Test
+    void findById() {
+        when(carClient.findById("2")).thenReturn(makeExpected());
+        var actualCar = carService.findById("2");
+        assertEquals(makeExpected(), actualCar);
+    }
+
+    private Car makeVictim() {
+        var car = new Car();
+        car.setId("1");
+        return car;
+    }
+
+    private Car makeExpected() {
+        var car = new Car();
+        car.setId("2");
+        car.setOwner("Master");
+        car.setVin("123456789");
+        car.setPlateNumber("1111");
+        car.setBrand("Toyota");
+        car.setModel("Corolla");
+        car.setGeneration("I");
+        car.setModification("B");
+        car.setEngine("Benzin");
+        car.setYear(2010);
+        return car;
+    }
+
+    private Car makeCarBeforeSave() {
+        var car = new Car();
+        car.setId("2");
+        return car;
+    }
+}
