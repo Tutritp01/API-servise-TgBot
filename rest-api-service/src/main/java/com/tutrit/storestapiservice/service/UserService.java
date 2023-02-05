38716e86f846fa633d4fc6aebf567aad916a7f44
AlexanderYarokhovich <yarokhovichalex@mail.ru>
2023-02-05 04:04:50 +0300

    Create User endpoint

---
+++ rest-api-service/src/main/java/com/tutrit/storestapiservice/service/UserService.java
@@ -0,0 +1,26 @@
+package com.tutrit.storestapiservice.service;
+
+
+import com.tutrit.persistence.core.bean.User;
+import com.tutrit.storestapiservice.client.UserClient;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserClient userClient;
+
+    public UserService(final UserClient userClient) {
+        this.userClient = userClient;
+    }
+
+    public User save(User user) {
+        user.setPhoneNumber("+375121212121");
+        userClient.save(user);
+        return user;
+    }
+
+    public User findById(String id) {
+        return userClient.findById(id);
+    }
+}
