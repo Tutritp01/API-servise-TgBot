4d5b176dfc6a15bfc5713cc3bd2cb327efb3ecf6
Vladimir Schneider <schneider_v_s@mail.ru>
2023-02-17 13:12:04 +0300

    TGBOT-280 Add UUIDWrapper Tests

---
+++ persistence-core/src/test/java/com/tutrit/persistence/core/service/UUIDWrapperTest.java
@@ -0,0 +1,19 @@
+package com.tutrit.persistence.core.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+class UUIDWrapperTest {
+    UUIDWrapper uuidWrapper = new UUIDWrapper();
+
+    @Test
+    void getID() {
+        String actualID = uuidWrapper.getID();
+        Assertions.assertNotNull(actualID);
+        Assertions.assertInstanceOf(String.class, actualID);
+    }
+
+    @Test
+    void getIdNotEquals() {
+        Assertions.assertNotEquals(uuidWrapper.getID(), uuidWrapper.getID());
+    }
+}
