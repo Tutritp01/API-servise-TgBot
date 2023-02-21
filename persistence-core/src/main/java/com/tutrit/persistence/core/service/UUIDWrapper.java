b32518409e9d92071938674921a6e84180a083c9
Vladimir Schneider <schneider_v_s@mail.ru>
2023-02-21 10:27:35 +0300

    TGBOT-280 Delete UUIDWrapper and test

--- persistence-core/src/main/java/com/tutrit/persistence/core/service/UUIDWrapper.java
+++
@@ -1,9 +0,0 @@
-package com.tutrit.persistence.core.service;
-
-import java.util.UUID;
-
-public class UUIDWrapper {
-    public String getID() {
-        return UUID.randomUUID().toString();
-    }
-}
