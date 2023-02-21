8d2de4624d438cf91433d7c51e1e39fa866b7dd0
Vladimir Schneider <schneider_v_s@mail.ru>
2023-02-21 10:20:39 +0300

    TGBOT-280 Fix EngineerPersistence (delete method update and add Optional in method findById)

--- persistence-core/src/main/java/com/tutrit/persistence/core/persistence/EngineerPersistence.java
+++ persistence-core/src/main/java/com/tutrit/persistence/core/persistence/EngineerPersistence.java
@@ -2,14 +2,14 @@ package com.tutrit.persistence.core.persistence;
 
 import com.tutrit.persistence.core.bean.Engineer;
 
+import java.util.Optional;
+
 public interface EngineerPersistence {
     Engineer save(Engineer engineer);
 
-    Engineer findById(String id);
+    Optional<Engineer> findById(String id);
 
     Iterable<Engineer> findAll();
-
-    Engineer update(String idOld, Engineer newEngineer);
 
     boolean delete(String id);
 }
