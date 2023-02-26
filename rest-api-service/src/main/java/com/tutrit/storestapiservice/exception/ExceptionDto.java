e7370abfbd9f681f6ec83092e73f35c30790c940
mikas <6666350@gmail.com>
2023-02-26 21:56:44 +0100

    removes demo data regarding error handling

--- rest-api-service/src/main/java/com/tutrit/storestapiservice/exception/ExceptionDto.java
+++
@@ -1,9 +0,0 @@
-package com.tutrit.storestapiservice.exception;
-
-public record ExceptionDto (
-    String timestamp,
-    String status,
-    String error,
-    String path,
-    String entityName
-){}
