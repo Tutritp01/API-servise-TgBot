5c20fb6ecbf4f80b37401744640fd74fe89c907d
“Artem <3947710@gmail.com>
2023-03-14 20:58:42 +0300

    [TGBOT-306] Create Integration module For API-service

---
+++ api-service-distributive/src/main/java/com/tutrit/app/ApplicationRun.java
@@ -0,0 +1,15 @@
+package com.tutrit.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.tutrit")
+public class ApplicationRun {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationRun.class, args);
+
+    }
+}
+
