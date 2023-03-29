c2bd56b0e3d4d2f1cd100f026ec509b5abb9d207
“Artem <3947710@gmail.com>
2023-03-29 19:25:35 +0300

    [TGBOT-353] adding SpringContext.java

---
+++ jdbc-persistence/src/test/java/com/tutrit/jdbc/config/SpringContext.java
@@ -0,0 +1,17 @@
+package com.tutrit.jdbc.config;
+
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+public class SpringContext {
+    @Configuration
+    @SpringBootConfiguration
+    @EnableAutoConfiguration
+    @ComponentScan(basePackages = "com.tutrit")
+    public static class SpringConfig {
+
+    }
+}
