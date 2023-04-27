6b5165bb67eb68c423d6e203a5a30c2f1223f170
“Artem <3947710@gmail.com>
2023-04-27 22:01:03 +0300

    [TGBOT-349] adding testContainer and profile with-integration-test

--- jdbc-persistence/src/test/java/com/tutrit/jdbc/repository/UserRepositoryIntegrationTest.java
+++ jdbc-persistence/src/test/java/com/tutrit/jdbc/repository/UserRepositoryIntegrationTest.java
@@ -2,30 +2,43 @@ package com.tutrit.jdbc.repository;
 
 import com.tutrit.jdbc.config.SpringContext;
 import com.tutrit.jdbc.entity.User;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.jdbc.Sql;
+import org.testcontainers.containers.MySQLContainer;
 
 import javax.sql.DataSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-
-@Sql(scripts = "classpath:/create-tables.sql")
+@Sql("classpath:create-tables.sql")
+@ActiveProfiles("IT")
 @SpringBootTest(classes = SpringContext.SpringConfig.class)
-@ActiveProfiles("test")
-class UserRepositoryIT {
+class UserRepositoryIntegrationTest {
+
     @Autowired
     private DataSource dataSource;
-
     private UserRepository userRepository;
-
     private User expectedUser;
+    public static final MySQLContainer<?> container = new MySQLContainer<>("mysql:8.0.33");
     private static final User user = new User("Jimi Hendrix", "555-12345");
+
+    @DynamicPropertySource
+    static void postgresProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", container::getJdbcUrl);
+    }
+
+    @BeforeAll
+    static void runContainer() {
+        container.start();
+    }
 
     @BeforeEach
     void setUp() {
@@ -35,7 +48,7 @@ class UserRepositoryIT {
 
     @Test
     void testSave() {
-        assertEquals(expectedUser, user);
+        assertEquals(user,expectedUser );
 
     }
 
@@ -43,12 +56,12 @@ class UserRepositoryIT {
     void testFindById() {
 
         User returnedUser = userRepository.findById(user.getUserId());
-        assertEquals(returnedUser, user);
+        assertEquals(user,returnedUser );
     }
 
     @Test
     void testUpdate() {
-        User freddyMercury = new User(user.getUserId(),"Freddy Mercury", "123-5555");
+        User freddyMercury = new User(user.getUserId(), "Freddy Mercury", "123-5555");
 
         userRepository.update(freddyMercury);
 
@@ -60,5 +73,4 @@ class UserRepositoryIT {
     void testDeleteById() {
         assertTrue(userRepository.deleteById(user.getUserId()));
     }
-
 }
