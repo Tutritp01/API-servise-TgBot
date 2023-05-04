4c782b473ad903b63b4b9f9ec784e04a4cd0bd89
“Artem <3947710@gmail.com>
2023-05-04 16:24:50 +0300

    [TGBOT-359] Adding User entity

---
+++ spring-data/src/main/java/com/tutrit/hibernate/entity/User.java
@@ -0,0 +1,35 @@
+package com.tutrit.hibernate.entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = IDENTITY)
+    private Long userId;
+
+    private String name;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    public User(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+}
