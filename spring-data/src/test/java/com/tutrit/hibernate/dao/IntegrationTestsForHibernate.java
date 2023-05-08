dddc5f5e0372342d88ab4c1362a7c3907a247619
“Artem <3947710@gmail.com>
2023-05-08 17:11:13 +0300

    [TGBOT-359] add IT tests

--- spring-data/src/test/java/com/tutrit/hibernate/dao/IntegrationTestsForHibernate.java
+++ spring-data/src/test/java/com/tutrit/hibernate/dao/IntegrationTestsForHibernate.java
@@ -1,16 +1,17 @@
-package com.tutrit.hibernate.repository;
+package com.tutrit.hibernate.dao;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.TestPropertySource;
 import org.testcontainers.containers.MySQLContainer;
-
+@TestPropertySource("classpath:hibernate-it.properties")
 public class IntegrationTestsForHibernate {
     public static final MySQLContainer<?> container = new MySQLContainer<>("mysql:8.0.33");
 
     @DynamicPropertySource
-    static void postgresProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", container::getJdbcUrl);
+    static void properties(DynamicPropertyRegistry registry) {
+        registry.add("hibernate.connection.url", container::getJdbcUrl);
     }
     @BeforeAll
     static void runContainer() {
