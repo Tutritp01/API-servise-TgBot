cc347e645e06ed3a786f7d101aea832c0f689373
“Artem <3947710@gmail.com>
2023-05-11 13:54:51 +0300

    [TGBOT-349] Refactoring

--- liquibase/src/main/resources/db/changelog/changes/001.add-User-table.sql
+++ liquibase/src/main/resources/db/changelog/changes/001.add-User-table.sql
@@ -2,10 +2,8 @@
 --changeset Artem_Metushko:1
 CREATE TABLE IF NOT EXISTS `users`
 (
-    `id`           BIGINT NOT NULL,
+    `user_id`           BIGINT NOT NULL,
     `name`         varchar(255) DEFAULT NULL,
     `phone_number` varchar(255) DEFAULT NULL,
-    PRIMARY KEY (`id`)
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_0900_ai_ci;
+    PRIMARY KEY (`user_id`)
+);
