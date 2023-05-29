fa62018fa26e54c40bdf99ba9f1fc0250c9fd360
yurasava <yurasava@gmail.com>
2023-05-29 21:37:53 +0300

    TGBOT-386 added cache to user service

--- api-service-distributive/src/main/java/com/tutrit/app/ApiServiceDistributive.java
+++ api-service-distributive/src/main/java/com/tutrit/app/ApiServiceDistributive.java
@@ -2,14 +2,14 @@ package com.tutrit.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableCaching
 @ComponentScan(basePackages = "com.tutrit")
 public class ApiServiceDistributive {
     public static void main(String[] args) {
         SpringApplication.run(ApiServiceDistributive.class, args);
-
     }
 }
-
--- rest-api-service/src/main/java/com/tutrit/storestapiservice/service/UserService.java
+++ rest-api-service/src/main/java/com/tutrit/storestapiservice/service/UserService.java
@@ -3,9 +3,11 @@ package com.tutrit.storestapiservice.service;
 
 import com.tutrit.persistence.core.bean.User;
 import com.tutrit.storestapiservice.client.UserClient;
+import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.stereotype.Service;
 
 @Service
+@CacheConfig(cacheNames = "users")
 public class UserService {
 
     private final UserClient userClient;
